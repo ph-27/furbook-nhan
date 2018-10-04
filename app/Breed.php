2833766179df52b5d443863870bc4ada5abfbbb2
NduyAT <duynhan.do.7@gmail.com>
2018-10-04 18:35:56 +0700

    asdasdas

---
+++ app/Breed.php
@@ -0,0 +1,13 @@
+<?php
+	namespace Furbook;
+
+	use Illuminate\Database\Eloquent\Model;
+
+	class Breed extends Model
+	{
+		public function cats(){
+			return $this -> hasMany('Furbook\Cat','breed_id','id');
+			
+		}
+	}
+?>
